4f9e7acacfb5c15da2d6f790cba225a20a6234fa
Viliam Kopecký <vk@viliamkopecky.cz>
2016-06-27 09:35:01 +0200

    add some more loaders

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -12,20 +12,36 @@ var config = {
 			test: /\.jsx?$/,
 			loaders: ['babel-loader'],
 			exclude: /node_modules/
-		}, {
+		},
+		{
 			test: /\.css$/,
 			loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
-		}, {
+		},
+		{
 			test: /\.styl$/,
 			loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus-loader'
-		}, {
-			test: /\.png|\.jpg|\.svg$/,
-			loaders: ['file-loader']
-		}]
+		},
+		{
+			test: /\.(png|jpg|jpeg|gif)$/,
+			loader: 'url-loader?limit=8192'
+		},
+		{
+			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "url-loader?limit=10000&minetype=application/font-woff"
+		},
+		{
+			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "file-loader"
+		},
+		{
+			test: /\.json$/,
+			loader: 'json',
+		}
+		]
 	},
 	stylus: {
 		use: [
-			poststylus([ 'autoprefixer' ])
+		poststylus([ 'autoprefixer' ])
 		]
 	},
 	output: {
