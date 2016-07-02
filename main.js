b90ac4dd387f81d5ba1853c37a1c967b8e0e63c5
Viliam Kopecký <vk@viliamkopecky.cz>
2016-07-02 15:21:19 +0200

    add devtools

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-const electron = require('electron')
+import electron from 'electron'
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer'
+
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
 function createWindow () {
-	mainWindow = new BrowserWindow({width: 800, height: 600})
+	mainWindow = new BrowserWindow({width: 800, height: 600, show: false})
+
+	mainWindow.setMenu(null)
 
 	mainWindow.loadURL(`file://${__dirname}/index.html`)
 
 	if(process.env.NODE_ENV === 'development') {
+		devtron.install()
+		installExtension(REACT_DEVELOPER_TOOLS)
+		installExtension(REDUX_DEVTOOLS)
 		mainWindow.webContents.openDevTools()
+	} else {
+		mainWindow.setAlwaysOnTop(true)
 	}
 
 	mainWindow.on('closed', function () {
 		mainWindow = null
+	})
+
+	mainWindow.once('ready-to-show', function () {
+		mainWindow.show()
 	})
 }
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 export default (initialState) => {
 	const sagaMiddleware = createSagaMiddleware();
-	const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware));
+	const store = createStore(rootReducer, initialState, compose(
+		applyMiddleware(sagaMiddleware),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
+	));
 	sagaMiddleware.run(rootSaga);
 	return store;
 };
